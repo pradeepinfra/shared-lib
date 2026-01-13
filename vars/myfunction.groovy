64702cd1843b709ee05dc9ef0951b619bda1e8f5
Infravyom IT Technologies <pradeep@infravyom.com>
2026-01-13 09:10:10 +0530

    Update myfunction.groovy

--- vars/myfunction.groovy
+++ vars/myfunction.groovy
@@ -1,3 +1,15 @@
 def call (){
   echo "hello, world"
 }
+
+def call() {
+    echo "✅ Maven Build Started..."
+
+    sh """
+        mvn -version
+        mvn clean package
+    """
+
+    echo "✅ Maven Build Completed..."
+}
+
